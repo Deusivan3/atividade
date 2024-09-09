9b3d1f6a33d5f3607dc2de5c8d6059207a689c33
Deusivan3 <deussilvapf10@gmail.com>
2024-09-08 22:36:21 -0300

    Add files via upload

---
+++ Atividade3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Atividade3 {
+    public string Nome;
+    public double SalarioBruto;
+    public double Imposto;
+
+    public double SalarioLiquido() {
+        return SalarioBruto - Imposto;
+    }
+
+    public void AumentarSalario(double porcentagem) {
+ SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+    public override string ToString() {
+    return Nome
+    + ", R$ "
+    + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+    }
+  }
+}
